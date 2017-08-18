76833451256ed16b692dd6d404d8110d10db4bf9
karenmcewen <karenf_email@yahoo.com>
2017-08-17 21:05:29 -0400

    second commit

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -10,6 +10,19 @@ namespace Conditionals
     {
         static void Main(string[] args)
         {
+            //Example 1 conditionals if/else
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+            if(age>=17)
+            {
+                Console.WriteLine("You can see the movie");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, you need a parent present.");
+
+            }
+
         }
     }
 }
