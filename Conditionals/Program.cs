e3c559485dca6fd610115ecd466e71b8c26a12c6
karenmcewen <karenf_email@yahoo.com>
2017-08-21 20:18:45 -0400

    age distribution

--- Conditionals/Program.cs
+++ Conditionals/Program.cs
@@ -51,7 +51,7 @@ namespace Conditionals
                 Console.WriteLine("I'm sorry, you need to try again");
             }
 
-       */
+     
             //Example 3 PROBLEM: At a restaurant, Mike and his three friends decided to divide the bill evenly. 
             //If each person paid $13 then what was the total bill?
             //Returning to the above problem, replace hard-coded values of 4(number of diners) and 13(cost per diner) 
@@ -86,8 +86,69 @@ namespace Conditionals
                 Console.WriteLine("Each person now owes $" + discountedBill5 / numDiners);
             }
 
+              */
 
+            //Example 3
+            /*You're responsible for providing a demographic report for your local school district based on age. 
+            To do this, you're going to determine which 'category' each person fits into based on their age. 
+            The person's age will determine which category they should be in:
+                If they're from 0 to 2 the child should be with parents print : 'Still in Mama's Arms' 
+                If they're 3 or 4 and should be in preschool print : 'Preschool Maniac' 
+                If they're from 5 to 11 and should be in elementary school print : 'Elementary School' 
+                From 12 to 14: 'Middle School' 
+                From 15 to 18: 'High School' 
+                From 19 to 22: 'College'
+                From 23 to 65: 'Working for the Man' 
+                From 66 to 100: 'The Golden Years' 
+            If the age of the person is less than 0 or more than 100 - it might be an alien - 
+                print: "This program is for humans".
+            */
 
+            Console.WriteLine("How old are you today?");
+            int personsAge = int.Parse(Console.ReadLine());
+
+            if (personsAge >=0 && personsAge <=2)
+            {
+                Console.WriteLine("'Still in Mama's Arms'");
+            }
+            else if (personsAge >= 3 && personsAge <= 4)
+            {
+                Console.WriteLine("'Preschool Maniac' ");
+            }
+
+            else if (personsAge >= 5 && personsAge <= 11)
+            {
+                Console.WriteLine("'Elementary School' ");
+            }
+
+            else if (personsAge >= 12 && personsAge <= 14)
+            {
+                Console.WriteLine("'Middle School' ");
+            }
+
+            else if (personsAge >= 15 && personsAge <= 18)
+            {
+                Console.WriteLine("'High School' ");
+            }
+            else if (personsAge >= 19 && personsAge <= 22)
+            {
+                Console.WriteLine("College");
+            }
+
+            else if (personsAge >= 23 && personsAge <= 65)
+            {
+                Console.WriteLine("Working for the man");
+            }
+
+            else if (personsAge >= 66 && personsAge <= 100)
+            {
+                Console.WriteLine("The Golden Years");
+            }
+
+            else
+            {
+                Console.WriteLine("This program is only for humans.");
+            }
         }
     }
 }
